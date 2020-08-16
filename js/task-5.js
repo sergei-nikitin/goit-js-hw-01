7dec6bc5b731a62368e0448f18ebf1933cb78b00
sergei-nikitin <65117368+sergei-nikitin@users.noreply.github.com>
2020-08-16 12:52:27 +0300

    16.08

--- js/task-5.js
+++ js/task-5.js
@@ -1,4 +1,23 @@
-const countryName = "КитаЙ";
+// Пользователь может оформить доставку товара к себе в страну,
+//   указав ее при посещении страницы в prompt.
+//     Учти, пользователь может ввести имя страны не только буквами нижнего регистра, а к примеру 'кИтАЙ'.
+
+// Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну.
+// Обязательно используй switch.
+// Формат сообщения: 'Доставка в [страна] будет стоить [цена] кредитов'.
+
+// Но доставка есть не везде, если указанной страны нет в списке,
+//   то выводи в alert сообщение 'В вашей стране доставка не доступна'.
+
+// Ниже приведен список стран и стоимость доставки.
+
+//   Китай - 100 кредитов
+// Чили - 250 кредитов
+// Австралия - 170 кредитов
+// Индия - 80 кредитов
+// Ямайка - 120 кредитов
+
+const countryName = "польша";
 
 const CANCELED_BY_USER = "Отменено пользователем!";
 const NO_DELIVERY = "В выбранную страну доставка недоступна.";
@@ -12,9 +31,8 @@ let country;
 if (countryName === null) {
   message = CANCELED_BY_USER;
 } else {
-  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); // Write code on this line
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
   switch (country) {
-    // Write code under this line
     case CHINA:
       price = 100;
       break;
@@ -32,7 +50,6 @@ if (countryName === null) {
   }
 }
 if (price > 0) {
-  // Write code on this line
   message = `Доставка в ${country} будет стоить ${price} кредитов`;
 }
 console.log(message);
